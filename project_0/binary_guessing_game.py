bdd3a7cb9c43d13396052f2d97601d96d2037c88
agabaevroman <roman@agabaev.ru>
2025-04-15 14:26:07 +0300

    Поменял архитектуру с учетом проекта коллаб

--- project_0/binary_guessing_game.py
+++ project_0/binary_guessing_game.py
@@ -3,6 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def binary_predict(number: int = 1) -> int:
+    """Угадываем число при помощи бинарного алгоритма с коррекцией
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """    
     count = 0
     low = 1
     high = 100
@@ -18,11 +26,40 @@ def binary_predict(number: int = 1) -> int:
     return count
 
 def random_predict(number: int = 1) -> int:
+    """Рандомно угадываем число
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """    
     count = 0
     while True:
         count += 1
         if number == np.random.randint(1, 101):
             break
+    return count
+
+def correction_predict(number: int = 1) -> int:
+    """Рандомно угадываем число при помощи коррекции
+
+    Args:
+        number (int, optional): _descriЗагаданное числоption_. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """    
+    count = 0
+    predict = np.random.randint(1, 101)
+
+    while number != predict:
+        count += 1
+        if number > predict:
+            predict += 1
+        elif number < predict:
+            predict -= 1
+
     return count
 
 def score_game(predict_func, seed: int = 1):
@@ -34,15 +71,17 @@ def score_game(predict_func, seed: int = 1):
 if __name__ == '__main__':
     random_attempts = score_game(random_predict, seed=42)
     binary_attempts = score_game(binary_predict, seed=42)
+    correction_attempts = score_game(correction_predict, seed=42)
 
     import pandas as pd
-    df = pd.DataFrame({'Random': random_attempts, 'Binary': binary_attempts})
+    df = pd.DataFrame({'Random': random_attempts, 'Correction': correction_attempts, 'Binary': binary_attempts})
 
     print("Среднее количество попыток:")
     print(df.mean())
 
     plt.figure(figsize=(10, 6))
     plt.hist(random_attempts, bins=range(1, max(random_attempts)+2), alpha=0.6, label='Random')
+    plt.hist(correction_attempts, bins=range(1, max(binary_attempts)+2), alpha=0.6, label='Prediction Search')
     plt.hist(binary_attempts, bins=range(1, max(binary_attempts)+2), alpha=0.6, label='Binary Search')
     plt.title('Распределение количества попыток угадывания числа')
     plt.xlabel('Количество попыток')
